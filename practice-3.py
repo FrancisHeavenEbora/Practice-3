21c6efcba68a4aa1777e4b16930be1657e49d9e0
Francis Heaven Ebora <113603180+FrancisHeavenEbora@users.noreply.github.com>
2022-09-17 14:41:14 +0700

    Add files via upload

---
+++ practice-3.py
@@ -0,0 +1,36 @@
+num1 = int(input("Enter First Numeber: "))
+
+num2 = int(input("Enter Second Number: "))
+
+
+
+print("Enter Which Operation Would You Like to Perform")
+
+op = input("Enter Operation That You Like +,-,*,/: ")
+
+
+
+result = 0
+
+if op == '+':
+
+  result = num1 + num2
+
+elif op == '-':
+
+  result = num1 - num2
+
+elif op == '*':
+
+  result = num1 * num2
+
+elif op == '/':
+
+  result = num1 / num2
+
+else:
+  print("INVALID")
+
+
+
+print(num1, op , num2, "=", result)
